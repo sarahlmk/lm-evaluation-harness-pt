a6e08cb22718b1c04de176becd231c09d08a48f1
Eduardo Garcia <edusantosgarcia@gmail.com>
2024-01-29 10:34:57 -0300

    When limit <=0 override config limit to None

--- lm_eval/api/task.py
+++ lm_eval/api/task.py
@@ -344,6 +344,8 @@ class Task(abc.ABC):
 
         if limit is None:
             limit = self.config.limit
+        if limit <= 0:
+            limit = None
         
         self.limit = limit
         self.config.limit = limit
